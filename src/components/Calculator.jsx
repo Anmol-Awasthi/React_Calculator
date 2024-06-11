ae14ef90ccbc22971ec0f2215db431623f56dd24
Anmol <anmolawasthi777@gmail.com>
2024-06-11 23:23:12 +0530

    Kya bolo

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,32 +2,26 @@ import { useState } from "react";
 import React from "react";
 import Display from "./Display";
 import ButtonsContainer from "./ButtonsContainer";
-import styles from "./Calculator.module.css";
 
 const calculator = () => {
-    
-    const [calVal, setCalVal] = useState("");
-    
+  const [calVal, setCalVal] = useState("");
 
-    const onButtonClick = (buttonText) => {
-      if(buttonText === "C") {
-        setCalVal("");
-      } else if(buttonText === "=") {
-        setCalVal(eval(calVal));
-      } else {
-        setCalVal(calVal + buttonText);
-      }
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      setCalVal(eval(calVal));
+    } else {
+      setCalVal(calVal + buttonText);
     }
+  };
 
-
-    return (
+  return (
     <>
-      <div className="calculator border-2 border-gray-300 rounded-lg max-w-full h-[calc(100vh)]  m-auto bg-[#243441] h-100vh md:max-w-[350px]" >
-        
+      <div className="calculator border-2 border-gray-300 rounded-lg max-w-full h-[calc(100vh)]  m-auto bg-[#243441] h-100vh md:max-w-[350px]">
         <Display displayValue={calVal} />
 
-        <ButtonsContainer onButtonClick = {onButtonClick} />
-        
+        <ButtonsContainer onButtonClick={onButtonClick} />
       </div>
     </>
   );
