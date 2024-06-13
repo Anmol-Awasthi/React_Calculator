543a120d478dfd545839740ea3afa1d4719945d7
Anmol <anmolawasthi777@gmail.com>
2024-06-14 00:00:17 +0530

    Major UI changes

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Display from "./Display";
 import ButtonsContainer from "./ButtonsContainer";
 
+
 const calculator = () => {
   const [calVal, setCalVal] = useState("");
 
